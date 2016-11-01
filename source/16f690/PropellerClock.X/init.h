84a63962d39f5bbe2d3cb31572dd9a2417c55692
大典 山根 <s15716@yonago.kosen-ac.jp>
2016-11-01 15:17:18 +0900

    PropellerClock本体用プロジェクトの作成

---
+++ source/16f690/PropellerClock.X/init.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   init.h
+ * Author: Yamane
+ *
+ * Created on 2016/11/01, 15:02
+ */
+
+#ifndef INIT_H
+#define	INIT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+extern void initPORT(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* INIT_H */
+
---
+++ source/16f690/PropellerClock.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PropellerClock.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PropellerClock.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=propellerclock.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/propellerclock.x.tar
